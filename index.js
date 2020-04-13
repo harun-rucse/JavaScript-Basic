7af73a66266f0a114c4cebf7df36f39e8fa490ff
Md Harun Or Rashid <horrashid007@gmail.com>
2020-04-13 13:02:47 +0600

    JavaScript This variable

--- index.js
+++ index.js
@@ -28,3 +28,78 @@ yearOfBirth.map((el, index, entierArr) => {
 const ages = yearOfBirth.map(el => new Date().getFullYear() - el);
 
 console.log(ages);
+
+
+////////////////////////////////////////
+// This variable
+console.log('--------------THIS VARIABLE--------------');
+
+/*
+ * in a regular function 'this' variable point to the Global Execution Context object which is window object
+ 
+ * in a method that attatch with an object 'this' variable point to the attatch object
+ */
+
+console.log(this);
+
+function fn() {
+    // This point window obj
+    console.log(this);
+}
+fn();
+
+const max = {
+    name: 'Max Smith',
+    yearOfBirth: 1988,
+    calcAge: function () {
+        //'this' point to the max object
+        console.log(this);
+    }
+}
+
+max.calcAge();
+
+
+const jane = {
+    name: 'Jane Smith',
+    yearOfBirth: 1998,
+    calcAge: function () {
+        //'this' point to the max object
+        console.log(this);
+
+        function inner() {
+            //inner() function is a regular function so 'this' point to the window object not the jane obj
+            console.log(this);
+        }
+        inner();
+    }
+}
+
+jane.calcAge();
+
+const anne = {
+    name: 'Anne',
+    yearOfBirth: 2000,
+    calcAge: function () {
+        //'this' point to the anne object
+        console.log(this);
+
+        function inner() {
+            //inner() function is a regular function so 'this' point to the window object not the anne obj
+            console.log(this);
+            const bob = {
+                name: 'Bob',
+                yearOfBirth: 1978,
+                calcAge: function () {
+                    //'this' point to the bob object
+                    console.log(this);
+                }
+            }
+            bob.calcAge();
+        }
+        inner();
+    }
+}
+
+// This point to the max object
+anne.calcAge();
